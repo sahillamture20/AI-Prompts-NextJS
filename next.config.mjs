95e416e735fc34118935f4a46482345955e1366f
sahillamture20 <sahillamture20@gmail.com>
2024-09-16 01:07:42 +0530

    try to change suspense build issue 1

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    // experimental: {
-    //   appDir: true,
-    //   serverComponentsExternalPackages: ["mongoose"],
-    // },
+    experimental: {
+      appDir: true,
+      serverComponentsExternalPackages: ["mongoose"],
+      missingSuspenseWithCSRBailout: false
+    },
     images: {  remotePatterns: [{
       protocol: 'https',
       hostname: 'lh3.googleusercontent.com',
